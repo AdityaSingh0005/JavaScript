e08dc6559944c1659a464480b55f820b1b9f8fbe
Aditya SIngh <as8916909@gmail.com>
2023-06-18 23:44:24 +0530

    Initial commit

---
+++ Day1.js
@@ -0,0 +1,21 @@
+console.log("This is Javascript Tutorial")
+// Variable in Javascropt
+// A Variable is a named storage for data 
+// We can use variables to store goodies, visitors, other data
+
+let message;  
+message = "How are you the future developer Aditya Singh"
+console.log(message);
+
+let user = 'aditya';
+let age = 20;
+let message1 = 'hello';
+console.log(user)
+
+let $ = 45
+var _ = 496
+console.log($+_)
+
+const mybirthday = '03.09.2003'
+let message2 = 'Happy Birthday'
+console.log(mybirthday + message2)
---
+++ Day2.js
@@ -0,0 +1,29 @@
+// Data Types in Javascript
+
+let message = 'hello'
+message = 4788.52
+console.log(message)
+
+let num = 45
+num = 78.25
+console.log(typeof(num))
+
+// There are 8 basic data types in javascript
+//  7 primitive data types
+
+// number for numbers of any kind: integer or floating-point, integers are limited by ±(253-1).
+// bigint for integer numbers of arbitrary length.
+// string for strings. A string may have zero or more characters, there’s no separate single-character type.
+// boolean for true/false.
+// null for unknown values – a standalone type that has a single value null.
+// undefined for unassigned values – a standalone type that has a single value undefined.
+// symbol for unique identifiers.
+
+// one non-primitive data type
+// object for more complex data structures
+
+
+let num1 = 4789
+let num2 = 478.54
+let sum = num1 + num2
+console.log(sum)
